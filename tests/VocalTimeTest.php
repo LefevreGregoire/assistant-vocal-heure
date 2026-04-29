506779ec507450cee90aa99705dcadf03d269391
LefevreGregoire <lefevregregoire@proton.me>
2026-04-29 14:09:04 +0200

    test: ajout du cas de l'après-midi

---
+++ tests/VocalTimeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\VocalTime;
+
+class VocalTimeTest extends TestCase
+{
+    public function testHeurePileMatin(): void
+    {
+        $this->assertEquals("sept heures du matin", VocalTime::traduire(new \DateTime('07:00')));
+        $this->assertEquals("une heure du matin", VocalTime::traduire(new \DateTime('01:00')));
+    }
+
+    public function testHeurePileApresMidi(): void
+    {
+        $this->assertEquals("deux heures de l'après-midi", VocalTime::traduire(new \DateTime('14:00')));
+    }
+}
