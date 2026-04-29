c7a6cf2d07a3c296846528d573b4e898388774c1
LefevreGregoire <lefevregregoire@proton.me>
2026-04-29 14:19:06 +0200

    feat/refactor: traduction des minutes de 05 à 30 avec un mapping dédié

--- src/VocalTime.php
+++ src/VocalTime.php
@@ -10,19 +10,30 @@ class VocalTime
         11 => 'onze'
     ];
 
+    private static array $minutesTexte = [
+        5 => 'cinq', 10 => 'dix', 15 => 'et quart',
+        20 => 'vingt', 25 => 'vingt-cinq', 30 => 'et demie'
+    ];
+
     public static function traduire(\DateTime $time): string
     {
         $heure = (int) $time->format('G');
         $minute = (int) $time->format('i');
-        
-        // Gestion des heures spéciales "pile"
-        if ($heure === 12 && $minute === 0) {
-            return "midi";
+
+        // Récupération du texte des minutes si elles existent
+        $strMinute = '';
+        if ($minute > 0 && isset(self::$minutesTexte[$minute])) {
+            $strMinute = ' ' . self::$minutesTexte[$minute];
         }
-        if ($heure === 0 && $minute === 0) {
-            return "minuit";
+
+        // Gestion de midi et minuit
+        if ($heure === 12) {
+            return "midi{$strMinute}";
         }
-        
+        if ($heure === 0) {
+            return "minuit{$strMinute}";
+        }
+
         $suffixe = "du matin";
         $heureAffichee = $heure;
 
@@ -30,10 +41,11 @@ class VocalTime
             $heureAffichee = $heure - 12;
             $suffixe = "de l'après-midi";
         }
-        
+
         $texteHeure = self::$nombres[$heureAffichee];
         $pluriel = $heureAffichee > 1 ? 's' : '';
-        
-        return "{$texteHeure} heure{$pluriel} {$suffixe}";
+
+        // On intercale les minutes avant le suffixe
+        return "{$texteHeure} heure{$pluriel}{$strMinute} {$suffixe}";
     }
 }
